f6f5018fe752a23b9cfbe6ff0dbfa470d5ba7566
Dr Walter Ngwenya <mrwalterngwenya@gmail.com>
2023-04-05 03:32:18 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_listint - prints all the elements of a listint_t list.
+ * this will print all the elements of a listint_t list.
  *
- * @h: head of linklist node
- *
- * Return: the number of nodes
  *
  */
 size_t print_listint(const listint_t *h)
